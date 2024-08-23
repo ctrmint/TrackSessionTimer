d164c72805cd865b8517b208d181fc514d3a8ad3
ctrmint <mark@mrodman.com>
2024-08-23 11:54:42 +0100

    Update touch_drive.py

--- touch_drive.py
+++ touch_drive.py
@@ -1,4 +1,5 @@
-#Touch drive
+# Touch drive
+# v3.1
 from machine import Pin,I2C,SPI,PWM,Timer,ADC
 import framebuf
 import time
@@ -181,11 +182,11 @@ class Touch_CST816T(object):
         LCD.show()
         
         
-    def GoScreen(self, LCD):
+    def GoScreen(self, LCD, text='..GO!'):
         #self.mode = 0
         #self.Set_Mode(self.Mode)
         LCD.fill(LCD.green)
-        LCD.write_text('..GO!',15,96,5,LCD.white)
+        LCD.write_text(text,12,96,4,LCD.white)
         LCD.show()
         time.sleep(1)
 
@@ -299,78 +300,7 @@ class Touch_CST816T(object):
         
 
 #######
-class QMI8658(object):
-    def __init__(self,address=0X6B):
-        self._address = address
-        self._bus = I2C(id=1,scl=Pin(I2C_SDL),sda=Pin(I2C_SDA),freq=100_000)
-        bRet=self.WhoAmI()
-        if bRet :
-            self.Read_Revision()
-        else    :
-            return NULL
-        self.Config_apply()
 
-    def _read_byte(self,cmd):
-        rec=self._bus.readfrom_mem(int(self._address),int(cmd),1)
-        return rec[0]
-    def _read_block(self, reg, length=1):
-        rec=self._bus.readfrom_mem(int(self._address),int(reg),length)
-        return rec
-    def _read_u16(self,cmd):
-        LSB = self._bus.readfrom_mem(int(self._address),int(cmd),1)
-        MSB = self._bus.readfrom_mem(int(self._address),int(cmd)+1,1)
-        return (MSB[0] << 8) + LSB[0]
-    def _write_byte(self,cmd,val):
-        self._bus.writeto_mem(int(self._address),int(cmd),bytes([int(val)]))
-        
-    def WhoAmI(self):
-        bRet=False
-        if (0x05) == self._read_byte(0x00):
-            bRet = True
-        return bRet
-    def Read_Revision(self):
-        return self._read_byte(0x01)
-    def Config_apply(self):
-        # REG CTRL1
-        self._write_byte(0x02,0x60)
-        # REG CTRL2 : QMI8658AccRange_8g  and QMI8658AccOdr_1000Hz
-        self._write_byte(0x03,0x23)
-        # REG CTRL3 : QMI8658GyrRange_512dps and QMI8658GyrOdr_1000Hz
-        self._write_byte(0x04,0x53)
-        # REG CTRL4 : No
-        self._write_byte(0x05,0x00)
-        # REG CTRL5 : Enable Gyroscope And Accelerometer Low-Pass Filter 
-        self._write_byte(0x06,0x11)
-        # REG CTRL6 : Disables Motion on Demand.
-        self._write_byte(0x07,0x00)
-        # REG CTRL7 : Enable Gyroscope And Accelerometer
-        self._write_byte(0x08,0x03)
-
-    def Read_Raw_XYZ(self):
-        xyz=[0,0,0,0,0,0]
-        raw_timestamp = self._read_block(0x30,3)
-        raw_acc_xyz=self._read_block(0x35,6)
-        raw_gyro_xyz=self._read_block(0x3b,6)
-        raw_xyz=self._read_block(0x35,12)
-        timestamp = (raw_timestamp[2]<<16)|(raw_timestamp[1]<<8)|(raw_timestamp[0])
-        for i in range(6):
-            # xyz[i]=(raw_acc_xyz[(i*2)+1]<<8)|(raw_acc_xyz[i*2])
-            # xyz[i+3]=(raw_gyro_xyz[((i+3)*2)+1]<<8)|(raw_gyro_xyz[(i+3)*2])
-            xyz[i] = (raw_xyz[(i*2)+1]<<8)|(raw_xyz[i*2])
-            if xyz[i] >= 32767:
-                xyz[i] = xyz[i]-65535
-        return xyz
-    def Read_XYZ(self):
-        xyz=[0,0,0,0,0,0]
-        raw_xyz=self.Read_Raw_XYZ()  
-        #QMI8658AccRange_8g
-        acc_lsb_div=(1<<12)
-        #QMI8658GyrRange_512dps
-        gyro_lsb_div = 64
-        for i in range(3):
-            xyz[i]=raw_xyz[i]/acc_lsb_div#(acc_lsb_div/1000.0)
-            xyz[i+3]=raw_xyz[i+3]*1.0/gyro_lsb_div
-        return xyz
 
 
 #Draw line and show 
@@ -520,3 +450,4 @@ def DOF_READ():
         LCD.show()
         if(Touch.Gestures == 0x0C):
             break
+
